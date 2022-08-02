666548f7e63249da5479f3e18a4d4f27d404c446
OlgaYakoko <108624217+OlgaYakoko@users.noreply.github.com>
2022-08-02 12:53:46 +0100

    second commit

--- js/script.js
+++ js/script.js
@@ -1,19 +1,41 @@
 'use strict';
 
-const firstUserNumber = prompt();
-const secondUserNumber = prompt();
+const multiplay = function (firstNumber, secondNumber) {
+  if( !isNaN(firstNumber) && !isNaN(secondNumber) && firstNumber != null && secondNumber != null){
+    const result = firstNumber * secondNumber;
+    return result;  
+  } else (console.log('err, enter your number'))  
+}
+
+const division = function (firstNumber, secondNumber) {
+  if( !isNaN(firstNumber) && !isNaN(secondNumber) && firstNumber != null && secondNumber != null){
+    const result = firstNumber / secondNumber;
+    return result;  
+  } else (console.log('err, enter your number'))  
+}
+
+const modulo = function (firstNumber, secondNumber) {
+  if( !isNaN(firstNumber) && !isNaN(secondNumber) && firstNumber != null && secondNumber != null){
+    const result = firstNumber % secondNumber;
+    return result;  
+  } else (console.log('err, enter your number'))  
+}
 
 const exponentiation = function (firstNumber, secondNumber) {
   if( !isNaN(firstNumber) && !isNaN(secondNumber) && firstNumber != null && secondNumber != null){
     const result = firstNumber ** secondNumber;
     return result;  
-  } else (console.log('err, enter your number'))
-    
+  } else (console.log('err, enter your number'))  
 }
 
 const mainFunction = function (callbackFunc) {
-    const resultFunc = exponentiation(firstUserNumber, secondUserNumber);
+    const firstUserNumber = prompt('your first number');
+    const secondUserNumber = prompt('your second number?');
+    const resultFunc = callbackFunc(firstUserNumber, secondUserNumber);
     return resultFunc;
 }
 
-alert(mainFunction(exponentiation));
+alert('result of exponentiation is ' + mainFunction(exponentiation));
+alert('result of multiply is ' + mainFunction(multiplay));
+alert('result of division is ' + mainFunction(division)); 
+alert('the result of the remainder of the division ' + mainFunction(modulo));
